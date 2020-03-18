f155de5454d5d9848f5a4c65eedd09a4fcbec46b
pao741 <anuvit741@gmail.com>
2020-03-18 13:48:11 +0700

    add hyperlink to html and create function in app.py

--- app.py
+++ app.py
@@ -3,15 +3,41 @@ import psycopg2
 conn = psycopg2.connect(database="postgres", user="postgres",
                         password="milanomilano", host="127.0.0.1", port="5432")
 print("Database Connected....")
+cur = conn.cursor()
+# cursor is used to execute query
+# cur.execute("select * from FROM name" +
+#             " WHERE table_name = transportation_name ")
+# station_names = []
+# row = cur.fetchall()
+# for row in rows:
+#    print "ID = ", row[0]
+#    print "NAME = ", row[1]
+#    station_names.append(row[1])
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    # return "i did it !"
-    # return "please"
+    # items = get_your_data_from_db()
+    # return render_template('index.html', station_names=station_names)
     return render_template('index.html')
+
+
+@app.route('/find_route')
+def find_route():
+    return render_template('find_route.html')
+
+
+@app.route('/can_reach')
+def can_reach():
+    return render_template('can_reach.html')
+
+
+@app.route('/all_routes')
+def all_routes():
+    return render_template('all_routes.html')
 
 
 if __name__ == "__main__":
