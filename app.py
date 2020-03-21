c598d6e83b5e21e2ffdab2b9b6e321e8e1cc4130
pao741 <anuvit741@gmail.com>
2020-03-21 16:21:50 +0700

    finish with html

--- app.py
+++ app.py
@@ -1,47 +1,62 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 import psycopg2
 conn = psycopg2.connect(database="postgres", user="postgres",
                         password="milanomilano", host="127.0.0.1", port="5432")
 print("Database Connected....")
 cur = conn.cursor()
-# cursor is used to execute query
-# cur.execute("select * from FROM name" +
-#             " WHERE table_name = transportation_name ")
-# station_names = []
-# cur.execute(
-#     "CREATE TABLE IF NOT EXISTS test_table (id integer, name varchar(20));")
-# cur.execute("DROP TABLE test_table")
 cur.execute(
-    "select table_name from information_schema.tables WHERE table_schema = 'public';")
+    "select stationName from station;")
 rows = cur.fetchall()
+stationName = []
 for row in rows:
-    print("ID = ", row[0])
-    # print("NAME = ", row[1])
-#    station_names.append(row[1])
+    stationName.append(row[0])
+stationName = sorted(stationName)
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    # items = get_your_data_from_db()
-    # return render_template('index.html', station_names=station_names)
     return render_template('index.html')
 
 
 @app.route('/find_route')
 def find_route():
-    return render_template('find_route.html')
+    return render_template('find_route.html', stationName=stationName)
+
+
+@app.route('/find_route', methods=['POST'])
+def find_route_post():
+    from_station = request.form['from']
+    to_station = request.form['to']
+    result = "find result"
+    return render_template('find_route.html', stationName=stationName, result=result)
 
 
 @app.route('/can_reach')
 def can_reach():
-    return render_template('can_reach.html')
+    return render_template('can_reach.html', stationName=stationName)
+
+
+@app.route('/can_reach', methods=['POST'])
+def can_reach_post():
+    from_station = request.form['from']
+    to_station = request.form['to']
+    result = "find result"
+    return render_template('can_reach.html', stationName=stationName, result=result)
 
 
 @app.route('/all_routes')
 def all_routes():
-    return render_template('all_routes.html')
+    return render_template('all_routes.html', stationName=stationName)
+
+
+@app.route('/all_routes', methods=['POST'])
+def all_routes_post():
+    from_station = request.form['from']
+    to_station = request.form['to']
+    result = "find result"
+    return render_template('all_routes.html', stationName=stationName, result=result)
 
 
 if __name__ == "__main__":
